b6fefa8d934c628b459a527c88fad698730cbb33
RoyWang <lanni0619@gmail.com>
2024-12-30 21:03:37 +0800

    Add new holder & get holder API

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 const express = require("express");
 const sequelize = require("./config/db");
 const Holders = require("./models/Holders");
+const query = require("./utils/query");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/v1/holders", async (req, res, next) => {});
-app.get("/api/v1/holder/:id", (req, res, next) => {});
+app.post("/api/v1/holders", async (req, res, next) => {
+  const { name, referrer, date } = req.body;
+
+  let position = null;
+  if (referrer) {
+    position = await query.findPosition(referrer);
+  }
+
+  const newHolder = await Holders.create({
+    name,
+    referrer,
+    date,
+  });
+
+  if (position) {
+    position.root[position.side] = newHolder.id;
+    await position.root.save();
+  }
+
+  res.status(201).json(newHolder);
+});
+
+app.get("/api/v1/holder/:id", async (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+
+  const subtreeData = await query.getSubtreeData(id);
+  if (!subtreeData) {
+    return res.status(404).json({ error: "Policyholder not found" });
+  }
+
+  res.status(200).json(subtreeData);
+});
 app.get("/api/v1/holders", (req, res, next) => {});
 
 app.get("/api/v1/reset", (req, res, next) => {
---
+++ utils/query.js
@@ -0,0 +1,52 @@
+const Holder = require("../models/Holders");
+
+// DFS
+async function getSubtreeCount(rootId) {
+  if (!rootId) return 0;
+  const root = await Holder.findByPk(rootId);
+  const leftCount = await getSubtreeCount(root.left_child);
+  const rightCount = await getSubtreeCount(root.right_child);
+  return 1 + leftCount + rightCount;
+}
+
+// Find new node's position
+async function findPosition(rootId) {
+  const root = await Holder.findByPk(rootId);
+  const leftCount = await getSubtreeCount(root.left_child);
+  const rightCount = await getSubtreeCount(root.right_child);
+
+  if (leftCount <= rightCount) {
+    if (!root.left_child) return { root, side: "left_child" };
+    return await findPosition(root.left_child);
+  } else {
+    if (!root.right_child) return { root, side: "right_child" };
+    return await findPosition(root.right_child);
+  }
+}
+
+async function getSubtreeData(id) {
+  if (!id) return null;
+
+  const node = await Holder.findByPk(id);
+  if (!node) return null;
+
+  const leftChild = await getSubtreeData(node.left_child);
+  const rightChild = await getSubtreeData(node.right_child);
+
+  return {
+    id: node.id,
+    name: node.name,
+    date: node.date,
+    referrer: node.referrer,
+    children: {
+      left: leftChild,
+      right: rightChild,
+    },
+  };
+}
+
+module.exports = {
+  findPosition,
+  getSubtreeData,
+};
+
